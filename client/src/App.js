de9dbddacfd63748e12118ff20d1faf67b12ddf2
Nemo <truongng201@gmail.com>
2023-09-09 21:17:44 +0700

    minor update

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project beta
         </a>
       </header>
     </div>
