3abbe77e6b76727786d61415771392e4c8cf64b3
Nemo <truongng201@gmail.com>
2023-09-09 22:01:13 +0700

    test ci cd

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Project beta 1.0.0
+          Project beta 1.0.1
         </a>
       </header>
     </div>
