f7c2f4d91e35db7bc3a7bf7aefae7ce5b0c5ed0b
Nemo <truongng201@gmail.com>
2023-09-09 16:33:52 +0700

    update ci cd for monitoring

---
+++ scripts/get-monitor-service-changed.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_FOLDER_NAME=$1
+
+# Get the current folder name
+echo $CURRENT_FOLDER_NAME
+SERVICES_CHANGED=()
+  # Check for changes in the folder name
+CHANGED=$(git diff --name-only HEAD~ HEAD | grep "$CURRENT_FOLDER_NAME")
+if [[ -n "$CHANGED" ]]; then
+  # read line separated by space into bash array 
+  while read -r line; do
+    # remove the folder name from the 
+    line=${line/$CURRENT_FOLDER_NAME\//}
+    # remove all the files from the path
+    line=${line/\/*/}
+    SERVICES_CHANGED+=("$line")
+  done <<< "$CHANGED"
+  echo ${SERVICES_CHANGED[@]}
+  exit 0
+else
+  exit 0
+fi
