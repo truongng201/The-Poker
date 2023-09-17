b75b71056167fab653b01f6f625a5fc87b784da0
Nemo <truongng201@gmail.com>
2023-09-18 01:18:42 +0700

    feat: update poker table

--- client/src/components/room/TablePoker.js
+++ client/src/components/room/TablePoker.js
@@ -12,9 +12,9 @@ export default function TablePoker() {
         <div className="card">5</div>
       </div>
       <div className="players">
-        <div className="player player-1">
+        <div className="player player-1 ">
           <div className="bank bank-up">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -24,11 +24,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=1"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-right">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-right">Messi</div>
         </div>
         <div className="player player-2">
           <div className="bank bank-down">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -38,11 +42,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=2"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-down">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-down">John</div>
         </div>
         <div className="player player-3">
           <div className="bank bank-down">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -52,11 +60,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=3"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-down">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-down">Mia</div>
         </div>
         <div className="player player-4">
           <div className="bank bank-down">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -66,11 +78,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=4"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-down">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-down">Terry</div>
         </div>
         <div className="player player-5">
           <div className="bank bank-down">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -80,11 +96,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=5"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-left">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-left">Halland</div>
         </div>
         <div className="player player-6">
           <div className="bank bank-up">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -94,11 +114,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=6"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-up">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-up">Mary</div>
         </div>
         <div className="player player-7">
           <div className="bank bank-up">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -108,11 +132,15 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=7"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-up">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-up">Tony</div>
         </div>
         <div className="player player-8">
           <div className="bank bank-up">
-            <div className="bank-value">1000000</div>
+            <div className="bank-value">1.000.000$</div>
             <div className="jetons v-10"></div>
             <div className="jetons v-5"></div>
             <div className="jetons v-1"></div>
@@ -122,7 +150,11 @@ export default function TablePoker() {
             alt="avatar"
             src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=8"
           />
-          <div className="player-name"></div>
+          <div className="player-group-cards player-group-cards-up">
+            <div className="player-card"></div>
+            <div className="player-card"></div>
+          </div>
+          <div className="player-name player-name-up">Henry</div>
         </div>
       </div>
     </div>
